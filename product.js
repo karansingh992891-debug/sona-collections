5d29c2f0c452439e39b7f2b33355b9e9190566f8
karansingh992891-debug <karansingh992891@gmail.com>
2026-07-25 12:01:32 +0530

    Add event listeners for buy and add to cart buttons

---
+++ product.js
@@ -0,0 +1,11 @@
+document.getElementById("buyNow").addEventListener("click",function(){
+
+alert("Checkout page coming soon.");
+
+});
+
+document.getElementById("addCart").addEventListener("click",function(){
+
+alert("Product Added To Cart");
+
+});
